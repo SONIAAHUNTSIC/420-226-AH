9ba94e9059d35fb64642a31ee4adafb86d0e99a2
Sonia Ibarra <Sonia-Adela.Ibarra-Flores.1@collegeahuntsic.qc.ca>
2016-04-04 12:42:54 -0400

    Initial Commit

---
+++ TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by SONIA IBARRA FLORES on 2016-04-04.
+//  Copyright © 2016 collegeahuntsic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by SONIA IBARRA FLORES on 2016-04-04.
+//  Copyright © 2016 collegeahuntsic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
